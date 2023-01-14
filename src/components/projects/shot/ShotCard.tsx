e5a252120c4725746e72009cc9d2e58122c83315
shinework <adrien.baptiste@gmail.com>
2023-01-14 18:17:15 +0100

    feat: add 4k support

--- src/components/projects/shot/ShotCard.tsx
+++ src/components/projects/shot/ShotCard.tsx
@@ -23,9 +23,26 @@ import { BsHeart, BsHeartFill } from "react-icons/bs";
 import { HiDownload } from "react-icons/hi";
 import { IoMdCheckmarkCircleOutline } from "react-icons/io";
 import { MdOutlineModelTraining } from "react-icons/md";
+import { Ri4KFill } from "react-icons/ri";
 import { useMutation, useQuery } from "react-query";
 import ShotImage from "./ShotImage";
 import { TbFaceIdError } from "react-icons/tb";
+
+const getHdLabel = (shot: Shot, isHd: boolean) => {
+  if (shot.hdStatus === "NO") {
+    return "Generate in 4K";
+  }
+
+  if (shot.hdStatus === "PENDING") {
+    return "4K in progress";
+  }
+
+  if (shot.hdStatus === "PROCESSED" && isHd) {
+    return "Show standard resolution";
+  }
+
+  return "Show 4K";
+};
 
 const ShotCard = ({
   shot: initialShot,
@@ -37,6 +54,7 @@ const ShotCard = ({
   const { onCopy, hasCopied } = useClipboard(initialShot.prompt);
   const { query } = useRouter();
   const [shot, setShot] = useState(initialShot);
+  const [isHd, setIsHd] = useState(Boolean(shot.hdOutputUrl));
 
   const { mutate: bookmark, isLoading } = useMutation(
     `update-shot-${initialShot.id}`,
@@ -46,6 +64,19 @@ const ShotCard = ({
         {
           bookmarked,
         }
+      ),
+    {
+      onSuccess: (response) => {
+        setShot(response.data.shot);
+      },
+    }
+  );
+
+  const { mutate: createdHd, isLoading: isCreatingHd } = useMutation(
+    `create-hd-${initialShot.id}`,
+    () =>
+      axios.post<{ shot: Shot }>(
+        `/api/projects/${query.id}/predictions/${initialShot.id}/hd`
       ),
     {
       onSuccess: (response) => {
@@ -65,10 +96,33 @@ const ShotCard = ({
     {
       refetchInterval: (data) => (data?.shot.outputUrl ? false : 5000),
       refetchOnWindowFocus: false,
-      enabled: !initialShot.outputUrl,
+      enabled: !initialShot.outputUrl && initialShot.status !== "failed",
       initialData: { shot: initialShot },
       onSuccess: (response) => {
         setShot(response.shot);
+      },
+    }
+  );
+
+  useQuery(
+    `shot-hd-${initialShot.id}`,
+    () =>
+      axios
+        .get<{ shot: Shot }>(
+          `/api/projects/${query.id}/predictions/${initialShot.id}/hd`
+        )
+        .then((res) => res.data),
+    {
+      refetchInterval: (data) =>
+        data?.shot.hdStatus !== "PENDING" ? false : 5000,
+      refetchOnWindowFocus: false,
+      enabled: shot.hdStatus === "PENDING",
+      initialData: { shot: initialShot },
+      onSuccess: (response) => {
+        setShot(response.shot);
+        if (response.shot.hdOutputUrl) {
+          setIsHd(true);
+        }
       },
     }
   );
@@ -82,7 +136,7 @@ const ShotCard = ({
       position="relative"
     >
       {shot.outputUrl ? (
-        <ShotImage shot={shot} />
+        <ShotImage isHd={isHd} shot={shot} />
       ) : (
         <Box>
           <AspectRatio ratio={1}>
@@ -104,10 +158,7 @@ const ShotCard = ({
         </Box>
       )}
       <Flex position="relative" p={3} flexDirection="column">
-        <Flex alignItems="center" justifyContent="space-between">
-          <Text color="blackAlpha.700" fontSize="xs">
-            {formatRelative(new Date(shot.createdAt), new Date())}
-          </Text>
+        <Flex alignItems="center" justifyContent="flex-end">
           <Box>
             {shot.seed && shot.outputUrl && (
               <Tooltip hasArrow label="Re-use style">
@@ -129,17 +180,41 @@ const ShotCard = ({
               </Tooltip>
             )}
             {shot.outputUrl && (
-              <IconButton
-                size="sm"
-                as={Link}
-                href={shot.outputUrl}
-                target="_blank"
-                variant="ghost"
-                aria-label="Download"
-                fontSize="md"
-                icon={<HiDownload />}
-              />
+              <>
+                <IconButton
+                  size="sm"
+                  as={Link}
+                  href={isHd ? shot.hdOutputUrl : shot.outputUrl}
+                  target="_blank"
+                  variant="ghost"
+                  aria-label="Download"
+                  fontSize="md"
+                  icon={<HiDownload />}
+                />
+                <Tooltip hasArrow label={getHdLabel(shot, isHd)}>
+                  <IconButton
+                    icon={<Ri4KFill />}
+                    color={isHd ? "red.400" : "gray.600"}
+                    isLoading={shot.hdStatus === "PENDING" || isCreatingHd}
+                    onClick={() => {
+                      if (shot.hdStatus === "NO") {
+                        createdHd();
+                      } else if (
+                        shot.hdStatus === "PROCESSED" &&
+                        shot.hdOutputUrl
+                      ) {
+                        setIsHd(!isHd);
+                      }
+                    }}
+                    size="sm"
+                    variant="ghost"
+                    aria-label="Make 4K"
+                    fontSize="lg"
+                  />
+                </Tooltip>
+              </>
             )}
+
             <Tooltip
               hasArrow
               label={`${shot.bookmarked ? "Remove" : "Add"} to your gallery`}
@@ -168,7 +243,10 @@ const ShotCard = ({
           {shot.prompt}
         </Text>
 
-        <HStack mt={4}>
+        <HStack justifyContent="space-between" mt={4}>
+          <Text color="beige.400" fontSize="xs">
+            {formatRelative(new Date(shot.createdAt), new Date())}
+          </Text>
           <Button
             rightIcon={hasCopied ? <IoMdCheckmarkCircleOutline /> : undefined}
             colorScheme="beige"
