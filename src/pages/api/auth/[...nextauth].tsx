1398f30018faeb065230b618cdbb38835394da10
shinework <adrien.baptiste@gmail.com>
2023-01-15 18:47:30 +0100

    fix: use next auth mailer

--- src/pages/api/auth/[...nextauth].tsx
+++ src/pages/api/auth/[...nextauth].tsx
@@ -2,8 +2,6 @@ import { PrismaAdapter } from "@next-auth/prisma-adapter";
 import NextAuth, { NextAuthOptions } from "next-auth";
 import EmailProvider from "next-auth/providers/email";
 import db from "@/core/db";
-import { EMAIL_SUBJECTS, sendEmail } from "@/core/mailer";
-import LoginEmail from "@/components/emails/LoginEmail";
 
 export const authOptions: NextAuthOptions = {
   adapter: PrismaAdapter(db),
@@ -11,13 +9,6 @@ export const authOptions: NextAuthOptions = {
     EmailProvider({
       server: process.env.EMAIL_SERVER,
       from: process.env.EMAIL_FROM,
-      sendVerificationRequest({ identifier: email, url }) {
-        sendEmail({
-          to: email,
-          subject: EMAIL_SUBJECTS.LOGIN,
-          component: <LoginEmail url={url} />,
-        });
-      },
     }),
   ],
   callbacks: {
